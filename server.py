35eb002232546515705d11d94e0b8a353b64b323
0xngmi <0xngmi@protonmail.com>
2022-12-03 23:00:13 +0000

    number conversion

--- server.py
+++ server.py
@@ -4,5 +4,5 @@ import os
 
 print(f"Serving on port {os.environ.get('PORT')}")
 
-http_server = WSGIServer(('', os.environ.get('PORT')), app)
+http_server = WSGIServer(('', int(os.environ.get('PORT'))), app)
 http_server.serve_forever()
